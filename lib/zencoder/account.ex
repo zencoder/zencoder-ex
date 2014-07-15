24849687f85933625dcbe0a6b41e5de5b866005b
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-07-14 19:09:11 -0500

    add account api

---
+++ lib/zencoder/account.ex
@@ -0,0 +1,20 @@
+defmodule Zencoder.Account do
+  import Zencoder.Resource
+
+  def create(options \\ %{}) do
+    post("/account", options)
+  end
+
+  def details(options \\ %{}) do
+    get("/account", options)
+  end
+
+  def integration(options \\ %{}) do
+    put("/account/integration", options)
+  end
+
+  def live(options \\ %{}) do
+    put("/account/live", options)
+  end
+
+end
